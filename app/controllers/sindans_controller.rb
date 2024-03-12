14308f2285135a69be070daae5a7600ea3ae4cb1
hINANO03 <21122226@st.yasuda-u.ac.jp>
2024-03-12 16:58:51 +0900

    first commit

---
+++ app/controllers/sindans_controller.rb
@@ -0,0 +1,27 @@
+class SindansController < ApplicationController
+    def index
+    end
+
+    def new
+        @sindan = Sindan.new
+    end
+
+    def show
+        @sindan = Sindan.find_by(id: params[:id])
+    end
+
+    def create
+        sindan = Sindan.new(sindan_params)
+        if sindan.save
+            flash[:notice] = "診断が完了しました"
+            redirect_to sindan_path(sindan.id)
+        else
+            redirect_to :action => "new"
+        end
+    end
+  
+  private
+    def sindan_params
+        params.require(:sindan).permit(:question1, :question2, :question3, :question4)
+    end
+end
---
+++ db/migrate/20240229011126_add_detail_to_posts.rb
@@ -0,0 +1,6 @@
+class AddDetailToPosts < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :overall, :integer
+    add_column :posts, :level, :integer　
+  end
+end
