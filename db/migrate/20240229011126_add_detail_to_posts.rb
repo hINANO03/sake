2e4ddfda43bdf87926a30bf99ae1e8ecf667deeb
hINANO03 <21122226@st.yasuda-u.ac.jp>
2024-03-12 17:48:02 +0900

    migrationfileの変更

--- db/migrate/20240229011126_add_detail_to_posts.rb
+++ db/migrate/20240229011126_add_detail_to_posts.rb
@@ -1,6 +1,6 @@
 class AddDetailToPosts < ActiveRecord::Migration[6.1]
   def change
     add_column :posts, :overall, :integer
-    add_column :posts, :level, :integer　
+    add_column :posts, :level, :integer
   end
 end
